018d83fb063a9f0580b9b7206982adc067c288bf
aaa <123>
2019-05-07 23:27:17 +0800

    1111

--- src/cn/itsource/Test1.java
+++ src/cn/itsource/Test1.java
@@ -3,6 +3,7 @@ package cn.itsource;
 public class Test1 {
     public static void main(String[] args) {
         int a=1;
+        int b=1;
         System.out.println("1111111111111111111111111");
     }
 }
